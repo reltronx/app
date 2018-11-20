a34959e7dedb2da975a133b52561af623d99dad3
mathuramg <mmg9291@gmail.com>
2018-11-20 23:08:50 +0530

    fetch users in the app

--- client/src/app/components/App/App.jsx
+++ client/src/app/components/App/App.jsx
@@ -123,6 +123,7 @@ class App extends React.Component {
     this.props.fetchCurrentUser()
       .then(() => {
         this.props.fetchUserPreferences();
+        this.props.fetchAllPages();
       });
   }
 
